288f1cf43e8b2089affb4f98220dd62021b1fa3c
Juan Reyes <jisaacrey011@gmail.com>
2026-03-12 20:35:00 -0400

    Juan

---
+++ DataAccess/Conexion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Reportes.DataAccess
+{
+    public class Conexion
+    {
+        // Lee la cadena de conexión del App.config
+        private static string cadena = ConfigurationManager
+            .ConnectionStrings["ReportesDB"].ConnectionString;
+
+        // Retorna una conexión abierta lista para usar
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection conexion = new SqlConnection(cadena);
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
